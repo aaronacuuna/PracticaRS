595b3fcb7c2ec99e82c8233d33b6b3f7eb6ef345
Pablo Romero <prg.pablo2004@gmail.com>
2025-12-15 20:21:13 +0100

    admin settings

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from models import Pelicula, Puntuacion, Genero
 # Register your models here.
+admin.site.register(Pelicula)
+admin.site.register(Puntuacion)
+admin.site.register(Genero)
