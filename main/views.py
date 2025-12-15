4cf476f891e6a5cdfa080ee9f124f68667dc359e
mpradoj04 <mprajim860@gmail.com>
2025-12-15 20:17:53 +0100

    recomend 3 films item-based

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import shelve
-from models import Puntuacion
-from main.recommendations import  transformPrefs, calculateSimilarItems
-from populateDB import populate_database
+
+
+from models import Puntuacion, Pelicula
+from main.recommendations import  transformPrefs, calculateSimilarItems, getRecommendedItems
+from main.forms import UsuarioBusquedaForm
+from django.conf import settings 
+from main.populateDB import populate_database
+
 
 # Create your views here.
 
@@ -33,3 +38,28 @@ def cargar_datos(request):
     if (peli, gen, us, punt) is not None:
         mensaje = "Base de datos poblada correctamente."
     return render(request, 'cargar_datos.html', {'peliculas': peli, 'generos': gen, 'usuarios': us, 'puntuaciones': punt, 'mensaje': mensaje})
+    
+def recomendar_peliculas_usuario_RSitems(request):
+    formulario = UsuarioBusquedaForm()
+    items = None
+    
+    if request.method=='POST':
+        formulario = UsuarioBusquedaForm(request.POST)
+        
+        if formulario.is_valid():
+            idUsuario=formulario.cleaned_data['idUsuario']
+            shelf = shelve.open("dataRS.dat")
+            Prefs = shelf['Prefs']
+            SimItems = shelf['SimItems']
+            shelf.close()
+            rankings = getRecommendedItems(Prefs, SimItems, int(idUsuario))
+            recomendadas= rankings[:3]
+            peliculas = []
+            puntuaciones = []
+            for re in recomendadas:
+                peliculas.append(Pelicula.objects.get(pk=re[1]))
+                puntuaciones.append(re[0])
+            items= zip(peliculas,puntuaciones)
+    
+    return render(request, 'recomendar_peliculas_usuarios.html', {'formulario':formulario, 'items':items, 'STATIC_URL':settings.STATIC_URL})
+
